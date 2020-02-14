f7398ca8f6edf8f19c634568ab720d0e2eeaac9d
Matthew Vernacchia <mvernacc@mit.edu>
2020-02-14 16:02:00 -0500

    Change quadratic form approximator to use Johnson SU distribution in the indefinite case

--- quadratic/quadratic.py
+++ quadratic/quadratic.py
@@ -1,9 +1,11 @@
 import numpy as np
 import scipy.stats
 from matplotlib import pyplot as plt
+from statsmodels.stats.moment_helpers import mnc2mc
 
 import cumulants_moments
 from poly_approx import poly_approx
+import johnson
 
 def approx_quad_form(mean: np.ndarray,
                      covar: np.ndarray, A: np.ndarray):
@@ -35,7 +37,23 @@ def approx_quad_form(mean: np.ndarray,
     else:
         # A is indefinite.
         # Support of Q is (-inf, +inf)
-        approx = poly_approx(moments, base='normal')
+        # Approximate with a Johnson SU distribution.
+
+        # Find central moments 0 to 4
+        moments = moments[:5]
+        central_moments = np.zeros(5)
+        central_moments[1:] = mnc2mc(moments[1:])
+        central_moments[0] = 1.
+        print(central_moments)
+
+        # Compute the parameters of the Johnson SU dist which has those moments
+        xi, lamb, gamma, delta = johnson.johnson_su_params_from_moments(
+            central_moments)
+        dist = scipy.stats.johnsonsu(
+            loc=xi, scale=lamb,
+            a=gamma, b=delta)
+        approx = dist.pdf
+
     return approx
 
 
@@ -115,6 +133,10 @@ def demo_indef():
     plt.ylabel('pdf(q) [-]')
     plt.legend()
 
+    central_moments_sample = scipy.stats.moment(
+        q_samples, moment=[0, 1, 2, 3, 4])
+    print(central_moments_sample)
+
 
 def demo_diag21():
     """A = diag([2, 1])"""
@@ -150,6 +172,6 @@ def demo_diag21():
 if __name__ == '__main__':
     # demo()
     # demo_neg()
-    # demo_indef()
-    demo_diag21()
+    demo_indef()
+    # demo_diag21()
     plt.show()
