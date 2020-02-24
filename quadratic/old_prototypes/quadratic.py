9b2606611b95173b8701c4892ffbed5481cfd2a1
Matthew Vernacchia <mvernacc@mit.edu>
2020-02-23 19:04:33 -0500

    misc updates to old prototype

--- quadratic/old_prototypes/quadratic.py
+++ quadratic/old_prototypes/quadratic.py
@@ -7,6 +7,7 @@ import cumulants_moments
 from poly_approx import poly_approx
 import johnson
 
+
 def approx_quad_form(mean: np.ndarray,
                      covar: np.ndarray, A: np.ndarray):
     """Approximate the probability density function of a
@@ -54,6 +55,32 @@ def approx_quad_form(mean: np.ndarray,
             a=gamma, b=delta)
         approx = dist.pdf
 
+    return approx
+
+
+def approx_quad_expr(mean: np.ndarray, covar: np.ndarray,
+                     A: np.ndarray, a: np.array, d: float):
+    if np.all(a == 0):
+        approx = approx_quad_form(mean, covar, A)
+        return lambda x: approx(x - d)
+
+    n_cumulants = 4
+    cumulants = np.zeros(n_cumulants)
+    for s in range(1, n_cumulants + 1):
+        cumulants[s - 1] = cumulants_moments.cumulant_quad_expr(
+            s, mean, covar, A, a)
+
+    moments = cumulants_moments.moments_from_cumulants(cumulants)
+    central_moments = np.zeros(5)
+    central_moments[1:] = mnc2mc(moments[1:])
+    central_moments[0] = 1.
+    # Compute the parameters of the Johnson SU dist which has those moments
+    xi, lamb, gamma, delta = johnson.johnson_su_params_from_moments(
+        central_moments)
+    dist = scipy.stats.johnsonsu(
+        loc=xi, scale=lamb,
+        a=gamma, b=delta)
+    approx = lambda x: dist.pdf(x - d)
     return approx
 
 
@@ -169,9 +196,51 @@ def demo_diag21():
     plt.legend()
 
 
+def demo_expr():
+    """Quadratic expression, with linear and constant terms."""
+    n_dim = 2
+    A = np.eye(n_dim)
+    A[1, 1] = -0.5
+    covar = np.eye(n_dim)
+    mean = np.zeros(n_dim)
+    a = np.ones(n_dim)
+    d = 0
+    approx = approx_quad_expr(
+        mean, covar, A, a, d)
+
+    # Sample from true dist
+    n_sample = int(1e5)
+    x = np.random.multivariate_normal(mean, covar, n_sample)
+    q_samples = np.zeros(n_sample)
+    for i in range(n_sample):
+        q_samples[i] = x[i] @ A @ x[i] + a @ x[i] + d
+
+    q = np.linspace(-10, 10)
+
+    plt.plot(
+        q, approx(q), label='Approx.',
+        color='tab:blue', linestyle='--')
+    bins = np.linspace(-10, 10, 101)
+    bins[0] = -np.inf
+    bins[-1] = np.inf
+    plt.hist(
+        q_samples, density=True, histtype='stepfilled',
+        bins=bins,
+        alpha=0.5, color='black', label='Samples')
+    plt.xlabel('q')
+    plt.ylabel('pdf(q) [-]')
+    plt.legend()
+
+    central_moments_sample = scipy.stats.moment(
+        q_samples, moment=[0, 1, 2, 3, 4])
+    print(central_moments_sample)
+
+
+
 if __name__ == '__main__':
     # demo()
     # demo_neg()
-    demo_indef()
+    # demo_indef()
     # demo_diag21()
+    demo_expr()
     plt.show()
