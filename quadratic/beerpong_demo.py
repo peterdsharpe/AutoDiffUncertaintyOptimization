369c1ec44b8c733e5a13b0dfae0b562502960a83
Matthew Vernacchia <mvernacc@mit.edu>
2020-02-24 14:04:21 -0500

    Add pretty plots to the beerpong demo.

--- quadratic/beerpong_demo.py
+++ quadratic/beerpong_demo.py
@@ -63,22 +63,49 @@ def plot_quad_approx():
     plt.ylabel('Range $r$ [m]')
     plt.ylim([0, plt.ylim()[1]])
     plt.legend()
+    plt.title('Range vs. launch angle')
+
+
+def plot_derivs():
+    """Plot the derivatives of the range function
+        wrt theta."""
+    theta = np.linspace(0, np.pi / 2)
+    v_init = 5.
+    range_true = projectile_range(v_init, theta)
+    jac = np.array([projectile_range_jac(v_init, t) for t in theta])
+
+    fig, axes = plt.subplots(
+        nrows=2, ncols=1, figsize=(6, 7),
+        sharex=True)
+    ax_dv = axes[0]
+    ax_dtheta = axes[1]
+    ax_dv.plot(
+        np.rad2deg(theta), jac[:, 0],
+        color='black', label='True range')
+    ax_dtheta.plot(
+        np.rad2deg(theta), jac[:, 1],
+        color='black', label='True range')
+    ax_dtheta.axhline(y=0, color='grey', zorder=0)
+    for ax in axes:
+        ax.legend()
+    ax_dtheta.set_xlabel('Launch angle $\\theta$ [deg]')
+    ax_dv.set_ylim([0, ax_dv.get_ylim()[1]])
+    ax_dv.set_ylabel('$dr / d v_{init}$ [s]')
+    ax_dtheta.set_ylabel('$dr / d\\theta$ [m rad$^{-1}$]')
+    plt.suptitle('Partial derivatives of range')
 
 
 def plot_range_pdf():
-    """TODO Something is very wrong!"""
     v_init = 5.
     sigma_v = 0.1
-    sigma_theta = np.deg2rad(5.)
+    sigma_theta = np.deg2rad(1.)
     covar = np.diag([sigma_v**2, sigma_theta**2])
+    mean = np.array([0., 0.])
 
     plt.figure()
 
     thetas = (np.pi / 6, np.pi / 4, np.pi / 3)
     for i, theta in enumerate(thetas):
-        # mean = np.array([v_init, theta])
-        mean = np.array([0., 0.])
-
         r_0 = projectile_range(v_init, theta)
         a = projectile_range_jac(v_init, theta)
         A = projectile_range_hes(v_init, theta)
@@ -88,7 +115,7 @@ def plot_range_pdf():
 
         r = np.linspace(0., 3.5, 100)
 
-        color  = 'C{:d}'.format(i)
+        color = 'C{:d}'.format(i)
         plt.plot(
             r, approx_dist.pdf(r),
             label='$\\theta = {:.0f}$ deg. (approx. dist.)'.format(
@@ -101,9 +128,52 @@ def plot_range_pdf():
     plt.ylabel('pdf($r$) [m$^{-1}$]')
     plt.ylim([0, plt.ylim()[1]])
     plt.legend()
+    plt.title('Range pdf for different launch angles')
+
+
+def plot_stddev_vs_launch_angle():
+    v_init = 5.
+    sigma_theta = np.deg2rad(1.)
+    mean = np.array([0., 0.])
+
+    plt.figure()
+    theta = np.linspace(0, np.pi / 2)
+
+    for iv, sigma_v in enumerate((0.1, 0.02)):
+        covar = np.diag([sigma_v**2, sigma_theta**2])
+        stddev_r = np.zeros(len(theta))
+
+        for i, theta_ in enumerate(theta):
+            r_0 = projectile_range(v_init, theta_)
+            a = projectile_range_jac(v_init, theta_)
+            A = projectile_range_hes(v_init, theta_)
+
+            approx_dist = quadratic.approx_quad_expr_nig(
+                mean, covar, A, a, r_0)
+            stddev_r[i] = approx_dist.std()
+
+        color = 'C{:d}'.format(iv)
+        plt.plot(
+            np.rad2deg(theta), stddev_r,
+            label='stddev($v_{{init}}$) = {:.2f} m s$^{{-1}}$'.format(
+                sigma_v),
+            color=color)
+
+    plt.xlabel('Launch angle $\\theta$ [deg]')
+    plt.ylabel('stddev($r$) [m]')
+    plt.ylim([0, plt.ylim()[1]])
+    plt.legend()
+    plt.title(
+        'Range standard deviation vs. launch angle, evaluated by quad. approx.\n'
+        + 'for different levels of initial velocity uncertainty\n'
+        + 'stddev($\\theta$) = {:.1f} deg'.format(
+            np.rad2deg(sigma_theta)))
+    plt.tight_layout()
 
 
 if __name__ == '__main__':
+    plot_derivs()
     plot_quad_approx()
     plot_range_pdf()
+    plot_stddev_vs_launch_angle()
     plt.show()
